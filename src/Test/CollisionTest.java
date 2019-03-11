db916f29f31ee62b42d9a9af4616ab97bf3aaec1
Dennis Larsson <tryckare@gmail.com>
2019-03-11 23:33:05 +0100

    collision test fungerar

--- src/Test/CollisionTest.java
+++ src/Test/CollisionTest.java
@@ -1,19 +1,39 @@
 package Test;
 
 import static org.junit.Assert.*;
+import game.*;
+import java.awt.Point;
+import java.awt.Rectangle;
 
 import org.junit.Before;
 import org.junit.Test;
 
+
+
+
 public class CollisionTest {
 
+	
+	 Collision bang;
+	
 	@Before
 	public void setUp() throws Exception {
+	bang = new Collision();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void falseTest() {
+		Rectangle r1 = new Rectangle(20, 30, 10, 10);
+		Rectangle r2 = new Rectangle(60, 60, 10, 10);
+		boolean actual = bang.between(r1, r2);
+		assertEquals(false, actual);
+	}
+	@Test
+	public void trueTest() {
+		Rectangle r1 = new Rectangle(20, 30, 10, 10);
+		Rectangle r2 = new Rectangle(20, 60, 10, 10);
+		boolean actual = bang.between(r1, r2);
+		assertEquals(false, actual);
 	}
 
 }
