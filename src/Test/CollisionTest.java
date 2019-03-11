3dd5347c28930c0b3875a17806ba239aed058324
Dennis Larsson <tryckare@gmail.com>
2019-03-11 21:11:14 +0100

    Ändrade namn på testklassen. Lättare om alla har samma mönster som tex klassensnamnTest

--- src/Test/CollisionTest.java
+++ src/Test/CollisionTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class Test_Collision {
+public class CollisionTest {
 
 	@Before
 	public void setUp() throws Exception {
