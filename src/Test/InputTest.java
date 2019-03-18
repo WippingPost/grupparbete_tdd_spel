d7bded4d0752b08ac9ccb5e85a81b8458cd9fb6f
Joakim Olofsson <joakim@joakimolofsson.com>
2019-03-18 20:16:07 +0100

    Ändrat test för inputmanager

--- src/Test/InputTest.java
+++ src/Test/InputTest.java
@@ -2,13 +2,27 @@ package Test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.awt.Component;
+import java.awt.Point;
+import java.awt.event.KeyEvent;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import game.Direction;
 import game.InputManager;
+import game.Player;
 
 public class InputTest {
 
+	
+	InputManager inputmanager;
+	
+	// Skapar en ny inputmanager
+	@Before
+	public void setUp() throws Exception {
+		inputmanager = new InputManager();
+	}
 	
 	/*
 	 * Testar Default Direction state för InputManager, när inputmanager körs ska denna som standard vara IDLE
@@ -18,12 +32,12 @@ public class InputTest {
 	public void testInputManagerDefaultState() {
 		// Act
 		Direction dir;
-		//KeyEvent e = KeyEvent.VK_RIGHT;
-		InputManager input = new  InputManager();
-		dir = input.getPlayerInput();
+		dir = inputmanager.getPlayerInput();
 		// Assert
 		assertEquals(Direction.IDLE, dir);
 		
 	}
+	
+
 	
 }
