16034c128aa8856ce8d88e3f8ee6691879efc0cc
Nicklas Holmberg <45942181+nishog04@users.noreply.github.com>
2019-03-21 22:17:38 +0100

    Lägger till det första testet gällande highscore.
    Här kontrolleras det huruvida det finns en highscore-fil. Om den inte existerar skapas en sådan fil.

---
+++ src/Test/HighscoreTest.java
@@ -0,0 +1,33 @@
+package Test;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.*;
+
+public class HighscoreTest {
+
+    @Test
+    public void checkForHighscoreFile(){
+        //Arrange
+        File file = new File("src/Test/txt/highscore.txt");
+        boolean exist;
+
+        // Act
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        exist = file.exists();
+
+        //Assert
+        assertTrue(exist);
+
+    }
+
+}
